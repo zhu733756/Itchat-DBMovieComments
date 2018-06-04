ec9f167d6c0fa8ee3afab7d9ead3ab26b37cf16a
zhu733756 <38685176+zhu733756@users.noreply.github.com>
2018-06-04 17:03:01 +0800

    Update load_douban_comments.py

--- load_douban_comments.py
+++ load_douban_comments.py
@@ -1,10 +1,19 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+"""
+-------------------------------------------------
+   File Name：     ProxyManager.py  
+   Description：
+-------------------------------------------------
+__author__ = 'ZH'
+"""
 import requests, re
 from bs4 import BeautifulSoup
 import logging, loggerConfig
 from pymongo import MongoClient
 import time, random
 
-loggerConfig.setup_logging(default_path="./log.yaml")
+loggerConfig.setup_logging(default_path="./log.yaml")#配置loger
 
 class get_douban_spider(object):
 
@@ -13,29 +22,39 @@ class get_douban_spider(object):
     def __init__(self, url):
 
         self.url = url
-        self.s = requests.session()
+        self.s = requests.session()#开启一个会话，爬取过多会提示盗链，需要cookie才能登录
         self.headers = {
             "User-Agent": \
                 "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"
         }
+        #读取本地cookie文件
         try:
             with open("./cookies.txt", "r") as f:
                 self.cookies = {i.split("=")[0]: i.split("=")[1] for i in f.read().split(";")}
         except Exception as e:
             self.logger.error("invalid cookies!:%s" % e)
+        #开启链接monogo数据库
         try:
             self.client = MongoClient("localhost:27017", connect=True)["testdb"]["DouBandb"]
         except Exception as e:
             self.logger.error("Failed to connect Mongodb:%s" % e)
 
     def html_parse(self, baseurl,params=None):
-
+        '''
+        开启会话登录cookie，请求页面，返回BeautifulSoup解析后的文本
+        :param baseurl:
+        :param params: params参数
+        :return:
+        '''
         content = self.s.get(baseurl, headers=self.headers, params=params, cookies=self.cookies).text
         comment_html = BeautifulSoup(content, "html.parser")
         return comment_html
 
     def get_page_comment(self):
-
+        '''
+        每一个参数字典对应一个url，请求对应url页面后解析需要的data存入mongodb
+        :return:
+        '''
         for params in self.get_pages():
             commentList=self.html_parse(self.url,params=params).find_all("div","comment")
             for commentInfo in commentList:
@@ -43,8 +62,8 @@ class get_douban_spider(object):
                     votes = commentInfo.find("span", "comment-vote").find("span", "votes").string
                     com_info = commentInfo.find("span", "comment-info")
                     name = com_info.find("a").string
-                    if self.check_name(name=name):
-                        self.logger.info("current data has inserted!")
+                    if self.check_name(name=name):#以name为index去重已经下载的数据
+                        self.logger.info("current data(name:%s) has inserted!"%name)
                         continue
                     href = com_info.find("a").get("href")
                     commentPeoInfo = self.html_parse(baseurl=href)
@@ -78,11 +97,18 @@ class get_douban_spider(object):
                 time.sleep(random.choice([5, 6, 7, 8, 9]))
 
     def check_name(self, name):
-
+        '''
+        查找数据库
+        :param name: 
+        :return: 
+        '''
         return True if self.client.find_one({"name": name}) else False
 
     def get_pages(self):
-
+        '''
+        根据评论数，指定参数字典中的start值，用生成器返回参数字典
+        :return: 
+        '''
         try:
             content = self.html_parse(self.url)
         except Exception as e:
@@ -96,7 +122,6 @@ class get_douban_spider(object):
                 yield params
         finally:
             self.s.close()
-
 
 if "__main__" == __name__:
     m = get_douban_spider("https://movie.douban.com/subject/6874741/comments")
