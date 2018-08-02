bc966d0b32b724038ab71d0df3504fa6ea4ccc51
zhu733756 <38685176+zhu733756@users.noreply.github.com>
2018-08-02 12:45:08 +0800

    Update kill_data.py

--- kill_data.py
+++ kill_data.py
@@ -2,7 +2,7 @@
 # !/usr/bin/env python
 """
 -------------------------------------------------
-   File Name：     ProxyManager.py  
+   File Name：     kill_data.py
    Description：
 -------------------------------------------------
 __author__ = 'ZH'
