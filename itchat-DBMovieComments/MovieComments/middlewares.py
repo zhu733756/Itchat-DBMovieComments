b767d8b5ef0b360874fd4bd0cc32d97879fab0bb
zhu733756 <1079333812@qq.com>
2018-08-27 16:56:23 +0800

    update cookiesmiddleware

--- itchat-DBMovieComments/MovieComments/middlewares.py
+++ itchat-DBMovieComments/MovieComments/middlewares.py
@@ -6,9 +6,13 @@
 # http://doc.scrapy.org/en/latest/topics/spider-middleware.html
 from scrapy import signals
 import logging,requests
-from random import choice
+from random import sample,choice
+import string
+import faker
+from scrapy.downloadermiddlewares.httpproxy import HttpProxyMiddleware
 
-class ProxyMiddleware():
+class ProxyMiddleware(object):
+
     def __init__(self, proxy_url):
         self.logger = logging.getLogger(__name__)
         self.proxy_url = proxy_url
@@ -23,18 +27,11 @@ class ProxyMiddleware():
             return False
 
     def process_request(self, request, spider):
-        # if request.meta.get('retry_times'):
         proxy = self.get_random_proxy()
         if proxy:
             uri = 'https://{proxy}'.format(proxy=proxy)
-            self.logger.debug('使用代理 ' + proxy)
+            self.logger.debug('使用代理:' + proxy)
             request.meta['proxy'] = uri
-        if request.meta.get('retry_times'):
-            proxy = self.get_random_proxy()
-            if proxy:
-                uri = 'https://{proxy}'.format(proxy=proxy)
-                self.logger.debug('使用代理 ' + proxy)
-                request.meta['proxy'] = uri
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -44,24 +41,12 @@ class ProxyMiddleware():
         )
 
 class CookiesMiddleware(object):
+    def get_bids(self):
+        bids="".join(sample(string.ascii_letters,11))
+        return {"bid":bids}
 
-    def __init__(self,cookie_str):
-        self.cookies_str=cookie_str
-        self.logger=logging.getLogger(__name__)
-
-    def get_cookies(self):
-        return {key.strip().split("=", 1)[0]: key.strip().split("=", 1)[1]
-                for key in self.cookies_str.strip().split(";")}
-
-    def process_request(self,request,spider):
+    def process_request(self, request, spider):
         self.logger.debug("use cookies")
-        request.cookies = self.get_cookies()
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        settings = crawler.settings
-        return cls(
-            cookie_str = settings.get("cookie_str"),
-        )
+        request.cookies =self.get_bids()
 
 
